e590a50599365da734cb4f332cc680676d9cd992
lautaroRondan <84650045+lautaroRondan@users.noreply.github.com>
2022-10-21 23:16:45 -0300

    Update global

--- src/helpers/Global.jsx
+++ src/helpers/Global.jsx
@@ -1,6 +1,6 @@
 const Global =  {
 //  url: "http://localhost:3900/api/"
-    url: "https://api-rest-blog01.herokuapp.com//api/"
+    url: "https://api-rest-blog01.herokuapp.com/api/"
 }
 
 export default Global
