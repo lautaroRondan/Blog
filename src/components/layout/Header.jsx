01854a45d22828bd266a631c11b11f62ab33d2dc
lautaro <lautarorondan96@gmail.com>
2022-11-03 17:39:25 -0300

    update

--- src/components/layout/Header.jsx
+++ src/components/layout/Header.jsx
@@ -1,10 +1,11 @@
 import React from 'react'
+import LR from '../../../public/LR.svg'
 
 const Header = () => {
   return (
     <header className="header">
             <div className="logo">
-                <div className="play"></div>
+            <img src={LR}/>
             </div>
             
             <h1>Mi Blog</h1>
