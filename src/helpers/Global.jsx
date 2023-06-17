d7067c08de6aa872d0600f1a39e31be9e6981d73
lautaroRondan <84650045+lautaroRondan@users.noreply.github.com>
2023-06-17 00:59:03 -0300

    Update Global.jsx

--- src/helpers/Global.jsx
+++ src/helpers/Global.jsx
@@ -1,6 +1,6 @@
 const Global =  {
 //  url: "http://localhost:3900/api/"
-    url: "https://api-rest-blog01.herokuapp.com/api/"
+    url: "https://api-rest-blog.vercel.app/api/"
 }
 
 export default Global
